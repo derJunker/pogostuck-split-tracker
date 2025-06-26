ebd6b94de94886814e619cc0a7298de010362bab
Jonas Heitz <mail@jonas-heitz.eu>
2025-06-26 14:50:48 +0200

    fix: some more details i might change later. just for testing

--- src/types/pogo-index-mapping.ts
+++ src/types/pogo-index-mapping.ts
@@ -21,30 +21,31 @@ const pogoIndexMapping: PogoLevelAndModes = {
             key: 0,
             name: "Map 1",
             checkpoints: [
-                { key: 0, name: "Checkpoint 1" },
-                { key: 1, name: "Checkpoint 2" },
-                { key: 2, name: "Checkpoint 3" },
-                { key: 3, name: "Checkpoint 4" },
-                { key: 4, name: "Checkpoint 5" }
+                { key: 0, name: "Bones" },
+                { key: 1, name: "Wind" },
+                { key: 2, name: "Grapes" },
+                { key: 3, name: "Tree" },
+                { key: 4, name: "Pineapples" }
             ],
             modes: [
                 { key: 0, name: "regular" },
+                { key: 1, name: "puzzle" },
+            ]
+        },
+        {
+            key: 1,
+            name: "Map 2",
+            checkpoints: [
+                { key: 0, name: "Ants" },
+                { key: 1, name: "Coconuts" },
+                { key: 2, name: "Clovers" },
+                { key: 3, name: "Ember" },
+                { key: 4, name: "Bees" }
+            ],
+            modes: [
+                { key: 0, name: "regular" },
+                { key: 1, name: "double jump" },
             ]
         }
     ]
-}
-
-export function getLevelNameByKey(key: number): string | undefined {
-    const level = pogoIndexMapping.level.find(l => l.key === key);
-    return level ? level.name : undefined;
-}
-
-export function getCheckpointNameByKey(levelKey: number, checkpointKey: number): string | undefined {
-    const level = pogoIndexMapping.level.find(l => l.key === levelKey);
-    return level ? level.checkpoints.find(c => c.key === checkpointKey)?.name : undefined;
-}
-
-export function getModeNameByKey(levelKey: number, modeKey: number): string | undefined {
-    const level = pogoIndexMapping.level.find(l => l.key === levelKey);
-    return level ? level.modes.find(m => m.key === modeKey)?.name : undefined;
 }

e2ad5d4cd73d1edec6ffd00e204f5077e7a0d119
Jonas Heitz <mail@jonas-heitz.eu>
2025-06-30 08:12:29 +0200

    fix: works with borderless pogostuck :D

--- src/main-process/split-overlay-window.ts
+++ src/main-process/split-overlay-window.ts
@@ -13,16 +13,17 @@ export function openOverlayWindow(mainWindow: BrowserWindow) {
         x: screen.getPrimaryDisplay().workArea.width - overlayWidth,
         y: 0,
         parent: mainWindow,
-        alwaysOnTop: true,
         transparent: true,
         frame: false,
         skipTaskbar: true,
+        focusable: false,
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
             preload: path.join(__dirname, "preload.js"),
         }
     });
+    overlayWindow.setAlwaysOnTop(true, "screen-saver")
 
     overlayWindow.on("ready-to-show", () => {
         overlayWindow.show();

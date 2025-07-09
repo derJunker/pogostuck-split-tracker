371fafb52c0f9737e15725463c5b372b1724d284
Jonas Heitz <jonas.heitz@msg.group>
2025-07-09 17:36:16 +0200

    fix: added error when systemRoot is not set. (which shouldnt be the case anyways)

--- src/main-process/windows11-listeners.ts
+++ src/main-process/windows11-listeners.ts
@@ -1,5 +1,7 @@
 import {ipcMain, shell} from 'electron';
 import * as os from 'os';
+import fs from "fs";
+import path from "path";
 
 export function initListeners() {
     ipcMain.handle('is-windows-11', () => {
@@ -10,8 +12,13 @@ export function initListeners() {
 
     ipcMain.handle('open-windows-settings',async () => {
         if (process.platform === 'win32') {
-            const systemRoot = process.env.SystemRoot || 'C://Windows';
-            await shell.openExternal(`${systemRoot}\\System32\\SystemPropertiesPerformance.exe`);
+            const systemRoot = process.env.SystemRoot || "C:\\Windows";
+            const settingsPath = path.join(systemRoot, 'System32', 'SystemPropertiesPerformance.exe');
+            if (!fs.existsSync(settingsPath)) {
+                console.error(`Settings path does not exist: ${settingsPath}`);
+                return;
+            }
+            await shell.openExternal(settingsPath);
         }
     });
 }
